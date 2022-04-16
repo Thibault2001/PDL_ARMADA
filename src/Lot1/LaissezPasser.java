fe3e926dea48590f9915c7681ade41582d11d2a5
Thibault2001 <thibault.serafini@groupe-esigelec.org>
2022-04-16 15:12:48 +0200

    Lot1

--- src/Lot1/LaissezPasser.java
+++ src/Lot1/LaissezPasser.java
@@ -1,6 +1,28 @@
 package Lot1;
 
+import java.util.*;
+import Lot1.*;
+
 public class LaissezPasser {
-	private String proprietaire;
+	private String nomLP;
+	private String prenomLP;
+	private String metierLP;
+	private String entrepriseLP;
+	private ArrayList <Vehicule> listeVehicule =new ArrayList <Vehicule> ();
+	private String plageHoraireLP;
+	private boolean valide;
+	private boolean archive;
+	
+	public LaissezPasser(String nomLP, String prenomLP, String metierLP, String entrepriseLP, ArrayList<Vehicule> listeVehicule, String plageHoraireLP, boolean valide, boolean archive) {
+		this.nomLP=nomLP;
+		this.prenomLP=prenomLP;
+		this.metierLP=metierLP;
+		this.entrepriseLP=entrepriseLP;
+		this.listeVehicule=listeVehicule;
+		this.plageHoraireLP=plageHoraireLP;
+		this.valide=valide;
+		this.archive=archive;
+		
+	}
 	
 }
---
+++ src/Lot1/Vehicule.java
@@ -0,0 +1,15 @@
+package Lot1;
+
+public class Vehicule {
+	private String idVehicule;
+	private String modele;
+	private float longueur;
+	private float largeur;
+	
+	public Vehicule(String idVehicule, String modele, float longueur, float largeur) {
+		this.idVehicule=idVehicule;
+		this.modele=modele;
+		this.longueur=longueur;
+		this.largeur=largeur;
+	}
+}
