6909c0d4acc9ebc08b87e758955e012281c91b87
Thibault2001 <thibault.serafini@groupe-esigelec.org>
2022-04-16 15:28:02 +0200

    Lot1

--- src/GUI/LaissezPasser/DemandeLP.java
+++ src/GUI/LaissezPasser/DemandeLP.java
@@ -3,10 +3,19 @@ package GUI.LaissezPasser;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class DemandeLP {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -38,6 +47,72 @@ public class DemandeLP {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Demande de laissez-passer\r\n");
+		lblNewLabel.setBounds(173, 34, 151, 31);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Veuillez remplir les champs ci-dessous :");
+		lblNewLabel_1.setBounds(34, 70, 219, 13);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Valider\r\n");
+		btnNewButton.setBounds(318, 232, 85, 21);
+		frame.getContentPane().add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(127, 106, 96, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Nom : \r\n");
+		lblNewLabel_2.setBounds(34, 109, 45, 13);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Pr\u00E9nom :\r\n");
+		lblNewLabel_2_1.setBounds(34, 132, 58, 13);
+		frame.getContentPane().add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("M\u00E9tier :");
+		lblNewLabel_2_2.setBounds(34, 155, 45, 13);
+		frame.getContentPane().add(lblNewLabel_2_2);
+		
+		JLabel lblNewLabel_2_3 = new JLabel("Entreprise :\r\n");
+		lblNewLabel_2_3.setBounds(34, 178, 45, 13);
+		frame.getContentPane().add(lblNewLabel_2_3);
+		
+		JLabel lblNewLabel_2_4 = new JLabel("V\u00E9hicule : ");
+		lblNewLabel_2_4.setBounds(34, 201, 45, 13);
+		frame.getContentPane().add(lblNewLabel_2_4);
+		
+		JLabel lblNewLabel_2_5 = new JLabel("Plage horaire (matin et/ou soir) :");
+		lblNewLabel_2_5.setBounds(34, 224, 45, 13);
+		frame.getContentPane().add(lblNewLabel_2_5);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(127, 129, 96, 19);
+		frame.getContentPane().add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(127, 152, 96, 19);
+		frame.getContentPane().add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(127, 175, 96, 19);
+		frame.getContentPane().add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(127, 198, 96, 19);
+		frame.getContentPane().add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(127, 221, 96, 19);
+		frame.getContentPane().add(textField_5);
 	}
-
 }
