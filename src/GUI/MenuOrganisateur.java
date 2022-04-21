2013f5c59eadf0e9471df7335f9aef691126f0a6
Thibault2001 <thibault.serafini@groupe-esigelec.org>
2022-04-21 23:21:32 +0200

    Lot1

---
+++ src/GUI/MenuOrganisateur.java
@@ -0,0 +1,82 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MenuOrganisateur {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuOrganisateur window = new MenuOrganisateur();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MenuOrganisateur() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Menu Organisateur\r\n");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel.setBounds(98, 25, 268, 35);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton GererLP = new JButton("G\u00E9rer les laissez-passer");
+		GererLP.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GererLP.setBounds(21, 95, 187, 44);
+		frame.getContentPane().add(GererLP);
+		
+		JButton GererInscription = new JButton("G\u00E9rer les Inscriptions");
+		GererInscription.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GererInscription.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GererInscription.setBounds(218, 95, 187, 44);
+		frame.getContentPane().add(GererInscription);
+		
+		JButton GererZone = new JButton("G\u00E9rer les Zones");
+		GererZone.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GererZone.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GererZone.setBounds(21, 171, 187, 44);
+		frame.getContentPane().add(GererZone);
+		
+		JButton GererParticipant = new JButton("G\u00E9rer les Participants");
+		GererParticipant.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GererParticipant.setBounds(218, 171, 187, 44);
+		frame.getContentPane().add(GererParticipant);
+	}
+
+}
---
+++ src/GUI/MenuParticipant.java
@@ -0,0 +1,57 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class MenuParticipant {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuParticipant window = new MenuParticipant();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MenuParticipant() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel MenuParticipant = new JLabel("Menu Participant");
+		MenuParticipant.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		MenuParticipant.setBounds(92, 21, 313, 63);
+		frame.getContentPane().add(MenuParticipant);
+		
+		JButton LP = new JButton("Laissez-Passer");
+		LP.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		LP.setBounds(139, 118, 150, 55);
+		frame.getContentPane().add(LP);
+	}
+
+}
